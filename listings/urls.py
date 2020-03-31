6a8c69fe2fade715f2c949db9200d4e6c18aed12
RdotSilva <RyandotSilva@gmail.com>
2020-03-31 19:20:30 -0400

    Add comment

--- listings/urls.py
+++ listings/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+# The 2nd argument of the path in the urlpatterns should match the method names created inside views.py
+# views.index = index
+# views.listing = listing
+
+
 urlpatterns = [
     path('', views.index, name='listings'),
     path('<int:listing_id>', views.listing, name='listing'),
