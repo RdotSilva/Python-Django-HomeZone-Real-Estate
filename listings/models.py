8884d8f3405b0ab8c027cf9fabffce94e7123330
RdotSilva <RyandotSilva@gmail.com>
2020-04-01 19:17:54 -0400

    Add Listing model

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from datetime import datetime
+from realtors.models import Realtor
 
-# Create your models here.
+
+class Listing(models.Model):
+    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
