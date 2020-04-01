31f371f859472f71c84b60a33c2fc1805b10cf1f
RdotSilva <RyandotSilva@gmail.com>
2020-04-01 19:19:46 -0400

    Add description fields

--- listings/models.py
+++ listings/models.py
@@ -4,9 +4,18 @@ from realtors.models import Realtor
 
 
 class Listing(models.Model):
+    # Do nothing if model is deleted
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     zipcode = models.CharField(max_length=20)
+    # blank=True makes field optional
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
+    garage = models.IntegerField(default=0)
+    sqft = models.IntegerField()
+    lot_size = models.DecimalField(max_digits=5, decimal_places=1)
