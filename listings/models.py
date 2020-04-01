8917c702cdf9e1689f38041c89ab57cb84ed14b0
RdotSilva <RyandotSilva@gmail.com>
2020-04-01 19:18:09 -0400

    Add location fields

--- listings/models.py
+++ listings/models.py
@@ -6,3 +6,7 @@ from realtors.models import Realtor
 class Listing(models.Model):
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=20)
