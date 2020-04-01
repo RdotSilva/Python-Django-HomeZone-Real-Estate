38d3ebb8a060b542a0556088ec546996c60d255d
RdotSilva <RyandotSilva@gmail.com>
2020-04-01 19:04:28 -0400

    Add postgres db config

--- hzrealestate/settings.py
+++ hzrealestate/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+# Imports from secret file, you need to create this and add your db info
+from secret import USERNAME, PASS
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -78,8 +81,11 @@ WSGI_APPLICATION = 'hzrealestate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'homezone',
+        'USER': USERNAME,
+        'PASSWORD': PASS,
+        'HOST': 'localhost'
     }
 }
 
