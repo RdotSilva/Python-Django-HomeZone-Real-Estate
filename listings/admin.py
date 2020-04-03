2c897de7fcb50cd3c770ce1296d0c5fe397ed68b
RdotSilva <RyandotSilva@gmail.com>
2020-04-02 20:14:54 -0400

    Add ListingAdmin class

--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Listing
 
-admin.site.register(Listing)
+
+# Define what you want to display in the admin table
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published',
+                    'price', 'list_date', 'realtor')
+
+
+admin.site.register(Listing, ListingAdmin)
