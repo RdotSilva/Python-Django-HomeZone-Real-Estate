17211710f8ecd08a84e393ecd4b3d5da602d6a1f
RdotSilva <RyandotSilva@gmail.com>
2020-04-02 20:32:05 -0400

    Add comments for options

--- listings/admin.py
+++ listings/admin.py
@@ -8,16 +8,21 @@ class ListingAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'is_published',
                     'price', 'list_date', 'realtor')
 
+    # Choose what items can be clicked as a link to navigate to listing
     list_display_links = ('id', 'title')
 
+    # Filter items
     list_filter = ('realtor',)
 
+    # Clickable published/unpublished
     list_editable = ('is_published',)
 
+    # Adds a search bar, these fields will be searchable
     search_fields = ('title', 'description', 'address',
                      'city', 'state', 'zipcode', 'price')
 
-    list_per_page = 2
+    # Pagination, number of listings per page
+    list_per_page = 25
 
 
 admin.site.register(Listing, ListingAdmin)
