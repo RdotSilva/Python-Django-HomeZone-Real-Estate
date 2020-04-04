4c500273ac66a1b1f9c7d3dc74f4ae9894625847
RdotSilva <RyandotSilva@gmail.com>
2020-04-03 20:15:23 -0400

    Create context & pass to render method

--- pages/views.py
+++ pages/views.py
@@ -8,8 +8,11 @@ from listings.models import Listing
 def index(request):
     # Fetch listings, order by list date desc, filter by published only. [:3] limits it to 3 listings only
     listings = Listing.order_by('-list_date').filter(is_published=True)[:3]
+    context = {
+        'listings': listings
+    }
 
-    return render(request, 'pages/index.html')
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
