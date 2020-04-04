5e6b977f0ba787d429790210cf150e3719feb7a2
RdotSilva <RyandotSilva@gmail.com>
2020-04-03 20:10:12 -0400

    Add result filter to index method

--- listings/views.py
+++ listings/views.py
@@ -9,8 +9,8 @@ from .models import Listing
 
 
 def index(request):
-    # Fetch all of the listings ordered by list date desc
-    listings = Listing.objects.order_by('-list_date')
+    # Fetch all of the listings ordered by list date desc. Filter results to published only.
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
     # Pagination
     paginator = Paginator(listings, 3)
