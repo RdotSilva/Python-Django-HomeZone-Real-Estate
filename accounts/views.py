839e2af05ceda04599f25253e673c2998bbb7fb8
RdotSilva <RyandotSilva@gmail.com>
2020-04-08 18:06:59 -0400

    Add login view method

--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def register(request):
     return render(request, 'accounts/register.html')
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
