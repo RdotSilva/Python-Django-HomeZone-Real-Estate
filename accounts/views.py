d0444841cca82f4959f6cc20a5ea6d18dc2a43b8
RdotSilva <RyandotSilva@gmail.com>
2020-04-10 20:08:41 -0400

    Add password validation

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
+
         # Get form values from request
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
@@ -11,6 +12,13 @@ def register(request):
         email = request.POST['email']
         password = request.POST['password']
         password2 = request.POST['password2']
+
+        # Check if passwords match
+        if password == password2:
+            return
+        else:
+            messages.error(request, 'Passwords must match')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
