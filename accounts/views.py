da4336f13e9f17c2ca4fbd47c0f76ce3b2170d0b
RdotSilva <RyandotSilva@gmail.com>
2020-04-10 20:20:48 -0400

    Add auto login after registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
 
 
@@ -25,8 +25,14 @@ def register(request):
                     messages.error(request, 'Email is taken')
                     return redirect('register')
                 else:
-                    # Looks good
-                    return
+                    # Create new user if validation passes
+                    user = User.objects.create_user(
+                        username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+
+                    # Auto login after register (uncomment below to add auto login)
+                    # auth.login(request, user)
+                    # messages.success(request, 'You are now logged in!')
+                    # return redirect('/index')
         else:
             messages.error(request, 'Passwords must match')
             return redirect('register')
