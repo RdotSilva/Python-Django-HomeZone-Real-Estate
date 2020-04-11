a337c85d86b8f8745891ee3af497d558eec2d062
RdotSilva <RyandotSilva@gmail.com>
2020-04-11 19:47:09 -0400

    Update login method to handle auth

--- accounts/views.py
+++ accounts/views.py
@@ -47,8 +47,19 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        # TODO: Add user login
-        return
+        # Get login details from post request
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Authenticate user
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in!')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
