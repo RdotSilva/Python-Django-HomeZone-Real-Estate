f69d8d7fcaad835974fa72001fd249961f954cd1
RdotSilva <RyandotSilva@gmail.com>
2020-04-10 20:01:04 -0400

    Add fade out effect

--- hzrealestate/static/js/main.js
+++ hzrealestate/static/js/main.js
@@ -1,2 +1,7 @@
 const date = new Date();
-document.querySelector('.year').innerHTML = date.getFullYear();
+document.querySelector(".year").innerHTML = date.getFullYear();
+
+// Used to create a fade out effect
+setTimeout(function () {
+  $("#message").fadeOut("slow");
+}, 3000);
