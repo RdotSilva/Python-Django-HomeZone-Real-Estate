c7f45874594fcecacf812a8123e338b92ba0749c
RdotSilva <RyandotSilva@gmail.com>
2020-04-10 20:06:42 -0400

    Add form values

--- accounts/views.py
+++ accounts/views.py
@@ -4,8 +4,13 @@ from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Testing error message')
-        return redirect('register')
+        # Get form values from request
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
     else:
         return render(request, 'accounts/register.html')
 
