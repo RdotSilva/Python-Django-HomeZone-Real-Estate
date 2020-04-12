87f33d55f0d28338ce283bd196fed87574535b33
RdotSilva <RyandotSilva@gmail.com>
2020-04-12 19:23:06 -0400

    Add __str__ method

--- contacts/models.py
+++ contacts/models.py
@@ -11,3 +11,6 @@ class Contact(models.Model):
     message = models.TextField(blank=True)
     contact_date = models.DateTimeField(default=datetime.now, blank=True)
     user_id = models.IntegerField(blank=True)
+
+    def __str__(self):
+        return self.name
