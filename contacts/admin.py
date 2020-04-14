56526edd178399ca73cb019a69a08f9fb7051f9a
RdotSilva <RyandotSilva@gmail.com>
2020-04-13 21:15:57 -0400

    Add list_display

--- contacts/admin.py
+++ contacts/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 from .models import Contact
 
 
-class ContactAdmin(admin.ModelAdmin)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'listing', 'email', 'contact_date')
+    list_display_links = ('id', 'name')
