02dbf111d549b85ca8f65508ede855bd49edaedb
RdotSilva <RyandotSilva@gmail.com>
2020-04-13 21:16:43 -0400

    Add search_fields

--- contacts/admin.py
+++ contacts/admin.py
@@ -6,3 +6,6 @@ from .models import Contact
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'listing', 'email', 'contact_date')
     list_display_links = ('id', 'name')
+    search_fields = ('name', 'email', 'listing')
+    list_per_page = 25
+
